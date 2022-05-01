fbf7b7eb07f1fe353535e3c97413cf0e90303408
KimJongHeon <klego98@gmail.com>
2022-05-01 18:35:11 +0900

    add minus friendly+ more confortable

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -28,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         Button btn2=findViewById(R.id.normal);
         Button btn3=findViewById(R.id.hard);
         Button btn4=findViewById(R.id.dilemma);
+        Button btn1F=findViewById(R.id.easyF);
+        Button btn2F=findViewById(R.id.normalF);
+        Button btn3F=findViewById(R.id.hardF);
+        Button btn4F=findViewById(R.id.dilemmaF);
         TextView level=findViewById(R.id.Lv);
         TextView xp=findViewById(R.id.exp);
         TextView friends=findViewById(R.id.friend);
@@ -36,71 +40,49 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                friendly=gain_friendly(0,true,friendly);
-                exp+=gain_exp(0,exp,friendly);
-                progressBar.setProgress((int)((exp/totalExp)*100));
-                xp.setText((int)exp+"/"+(int)totalExp);
-                friends.setText(friendly+"");
-                if(progressBar.getProgress()==100){
-                    exp-=totalExp;
-                    totalExp*=1.2;
-                    charLv++;
-                    level.setText(charLv+"");
-                    progressBar.setProgress((int)((exp/totalExp)*100));
-                    xp.setText((int)exp+"/"+(int)totalExp);
-
-                }
+                react_exp(progressBar,xp,friends,level,0,true);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                friendly=gain_friendly(1,true,friendly);
-                exp+=gain_exp(1,exp,friendly);
-                progressBar.setProgress((int)((exp/totalExp)*100));
-                xp.setText((int)exp+"/"+(int)totalExp);
-                if(progressBar.getProgress()==100){
-                    exp-=totalExp;
-                    totalExp*=1.2;
-                    charLv++;
-                    level.setText(charLv+"");
-                    progressBar.setProgress((int)((exp/totalExp)*100));
-                    xp.setText((int)exp+"/"+(int)totalExp);
-                }
+                react_exp(progressBar,xp,friends,level,1,true);
             }
         });
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                friendly=gain_friendly(2,true,friendly);
-                exp+=gain_exp(2,exp,friendly);
-                progressBar.setProgress((int)((exp/totalExp)*100));
-                xp.setText((int)exp+"/"+(int)totalExp);
-                if(progressBar.getProgress()==100){
-                    exp-=totalExp;
-                    totalExp*=1.2;
-                    charLv++;
-                    level.setText(charLv+"");
-                    progressBar.setProgress((int)((exp/totalExp)*100));
-                    xp.setText((int)exp+"/"+(int)totalExp);
-                }
+                react_exp(progressBar,xp,friends,level,2,true);
             }
         });
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                friendly=gain_friendly(3,true,friendly);
-                exp+=gain_exp(3,exp,friendly);
-                progressBar.setProgress((int)((exp/totalExp)*100));
-                xp.setText((int)exp+"/"+(int)totalExp);
-                if(progressBar.getProgress()==100){
-                    exp-=totalExp;
-                    totalExp*=1.2;
-                    charLv++;
-                    level.setText(charLv+"");
-                    progressBar.setProgress((int)((exp/totalExp)*100));
-                    xp.setText((int)exp+"/"+(int)totalExp);
-                }
+                react_exp(progressBar,xp,friends,level,3,true);
+            }
+        });
+        btn1F.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                react_exp(progressBar,xp,friends,level,0,false);
+            }
+        });
+        btn2F.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                react_exp(progressBar,xp,friends,level,1,false);
+            }
+        });
+        btn3F.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                react_exp(progressBar,xp,friends,level,2,false);
+            }
+        });
+        btn4F.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                react_exp(progressBar,xp,friends,level,3,false);
             }
         });
     }
@@ -116,6 +98,21 @@ public class MainActivity extends AppCompatActivity {
         else
             exp=0;
         return exp;
+    }
+    public void react_exp(ProgressBar progressBar,TextView xp, TextView friends, TextView level,int achieve,boolean success){
+        friendly=gain_friendly(achieve,success,friendly);
+        exp+=gain_exp(achieve,exp,friendly);
+        progressBar.setProgress((int)((exp/totalExp)*100));
+        xp.setText((int)exp+"/"+(int)totalExp);
+        friends.setText(friendly+"");
+        if(progressBar.getProgress()==100){
+            exp-=totalExp;
+            totalExp*=1.2;
+            charLv++;
+            level.setText(charLv+"");
+            progressBar.setProgress((int)((exp/totalExp)*100));
+            xp.setText((int)exp+"/"+(int)totalExp);
+        }
     }
     public int gain_friendly(int achieve,boolean success,int friendly) {
         if(success==true) {
@@ -142,9 +139,11 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-        if(friendly>100){
+        if(friendly>100){//most friendly is 100
             friendly=100;
         }
+        if(friendly<-100)//less friendly is -100
+            friendly=-100;
         return friendly;
     }
 
