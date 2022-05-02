53fefc42f00f3668ad39b43698777c090c375498
Insiro <leeon@insiro.me>
2022-05-02 15:40:07 +0900

    add: Auth Activity

---
+++ app/src/main/java/com/insiro/lifepet/authy/Register.kt
@@ -0,0 +1,12 @@
+package com.insiro.lifepet.authy
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Register:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
