b7d029e363ef9500522dd718e1d6818e3f688cd2
Insiro <leeon@insiro.me>
2022-05-02 16:27:44 +0900

    add: achivement entity

---
+++ entity/src/main/java/com/insiro/lifepet/entity/AchivementCategory.kt
@@ -0,0 +1,14 @@
+package com.insiro.lifepet.entity
+
+import com.insiro.lifepet.entity.User
+
+class AchivementCategory {
+    private lateinit var id:String
+    private lateinit var name:String
+    private lateinit var desc:AchivementCategory
+
+    constructor(){
+
+    }
+
+}
