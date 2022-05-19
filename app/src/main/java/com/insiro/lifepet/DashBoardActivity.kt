2b231276ef4c8cb7e9514445fd102023be6d8d98
Insiro <leeon@insiro.me>
2022-05-19 16:10:30 +0900

    add:Sign checker with intent

---
+++ app/src/main/java/com/insiro/lifepet/DashBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.insiro.lifepet
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class DashBoardActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("adas","ADdas")
+    }
+}
--- app/src/main/java/com/insiro/lifepet/MainActivity.kt
+++ app/src/main/java/com/insiro/lifepet/MainActivity.kt
@@ -1,16 +1,55 @@
 package com.insiro.lifepet
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock.sleep
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.insiro.lifepet.authy.Auth
+import com.insiro.lifepet.dataManager.*
+import com.insiro.lifepet.entity.UserFull
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var activityResultRancher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        activityResultRancher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                responseUser(it)
+            }
+        val intent = Intent(this, DataManager::class.java)
+        val bundle = QueryBundleBuilder()
+            .addQuery(Query(Field.User, Action.Activate))
+            .addQuery(Query(Field.User, Action.Get))
+            .build()
+        intent.putExtras(bundle)
+        activityResultRancher.launch(intent)
+
     }
 
+    private fun responseUser(result: ActivityResult) {
+        val bundle = result.data?.extras
+        var intent = Intent(this, Auth::class.java)
+        if (bundle != null) {
+            val reader = ResponseBundleReader(bundle)
+            val receiveData = reader.getData(false)
+            if (receiveData != null) {
+                val data = receiveData.data as UserFull?
+                if (data != null) {
+                    intent = Intent(this, DashBoardActivity::class.java)
 
+                }
+            }
+        }
+        sleep(2000)
+
+        startActivity(intent)
+        finish()
+        return
+    }
 
 
 }
