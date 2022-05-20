da3c33264b11cd96c5541c9296d5a5aaa686f470
Insiro <leeon@insiro.me>
2022-05-20 17:52:01 +0900

    add schedule detail base

---
+++ app/src/main/java/com/insiro/lifepet/Schedule/ScheduleDetail.kt
@@ -0,0 +1,15 @@
+package com.insiro.lifepet.Schedule
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.insiro.lifepet.databinding.ActivityScheduleBinding
+
+class ScheduleDetail : AppCompatActivity() {
+    private val scheduleBinding: ActivityScheduleBinding =
+        ActivityScheduleBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
